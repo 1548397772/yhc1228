2e592f55004b00332fd274d010bf8013829fc4b9
1548397772 <xsb537@163.com>
2016-12-28 09:09:38 +0800

    Initial Commit

---
+++ 1228/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1228
+//
+//  Created by BJyhc on 2016/12/28.
+//  Copyright © 2016年 blue@circle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
